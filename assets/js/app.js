9dc30cd4e060ba1167eb25e39039d640a82016aa
Gareth Johns <info@garethjohnsdesign.com>
2016-10-18 21:49:19 +0100

    Updated Hover and 15 other files

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,12 +6,7 @@ $('.gallery').flickity({
   // options
   imagesLoaded: true,
   setGallerySize: false,
-  arrowShape: {
-      x0: 10,
-  x1: 60, y1: 50,
-  x2: 60, y2: 40,
-  x3: 20
-    }
+  arrowShape: 'M100,46.875H11.9688l17.4688-17.4688L25,25L0,50l25,25l4.4062-4.4062L11.9688,53.125H100V46.875z'
 });
 
 
