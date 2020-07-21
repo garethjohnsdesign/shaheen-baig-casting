377b9c548318ac28b5b4da91f67b04b8745acd58
Gareth Johns <garethjohns@Gareths-iMac.local>
2020-07-21 15:54:36 +0100

    Initial Commit

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -59,6 +59,8 @@ new Swiper('#carousel--hero', {
 // 4. Plyr
 // ----------
 
+const players = Plyr.setup('.js-player');
+
 const player = new Plyr('#player', {
   ratio: '16:9',
   controls: ['play', 'progress', 'mute', 'volume', 'pip', 'airplay', 'fullscreen']
